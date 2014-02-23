7a0aba16fa3f27974b95f5889d4fdc66d06ef796
Gordon McCreight <gordon@mccreight.com>
2014-02-22 18:36:19 -0800

    refactor the AsanaProjectUpdater class

--- lib/epicify/asana_project_updater.rb
+++ lib/epicify/asana_project_updater.rb
@@ -11,31 +11,45 @@ module Epicify
 
       @project.sections.each do |section|
         if section.is_overview?
-          if @project.report_overview_name != @project.overview_section.name
-            @asana_adapter.update_task_name section.id, @project.report_overview_name
-          end
+          _update_overview_section(section)
         else
-          original_name = section.name()
-          new_name = section.report_section_name()
-          if new_name != original_name
-            @asana_adapter.update_task_name section.id, new_name
-          end
+          _update_normal_section(section)
         end
 
         section.stories.each do |story|
-          if story.name_was_changed?
-            @asana_adapter.update_task_name story.id, story.name
-          end
-          story.added_tags.each do |tag|
-            @asana_adapter.associate_tag_with_task tag.id, story.id
-          end
-          story.removed_tags.each do |tag|
-            @asana_adapter.remove_tag_from_task tag.id, story.id
-          end
+          _update_story(story)
         end
       end
 
     end
+
+    private
+
+      def _update_overview_section(overview_section)
+        if @project.report_overview_name != @project.overview_section.name
+          @asana_adapter.update_task_name overview_section.id, @project.report_overview_name
+        end
+      end
+
+      def _update_normal_section(section)
+        original_name = section.name()
+        new_name = section.report_section_name()
+        if new_name != original_name
+          @asana_adapter.update_task_name section.id, new_name
+        end
+      end
+
+      def _update_story(story)
+        if story.name_was_changed?
+          @asana_adapter.update_task_name story.id, story.name
+        end
+        story.added_tags.each do |tag|
+          @asana_adapter.associate_tag_with_task tag.id, story.id
+        end
+        story.removed_tags.each do |tag|
+          @asana_adapter.remove_tag_from_task tag.id, story.id
+        end
+      end
 
   end
 
