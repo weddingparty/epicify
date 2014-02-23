d25e2e233d96aac7cb452d48d6ef79400c59410f
Gordon McCreight <gordon@mccreight.com>
2014-02-23 09:28:57 -0800

    Heavily refactor tags

--- lib/epicify/story.rb
+++ lib/epicify/story.rb
@@ -48,7 +48,7 @@ module Epicify
           num_hyphens = matcher[1].delete(' ').size
           self.name = matcher[3]
           _remove_any_preexisting_points_tags_from_task
-          _add_tag_with_value_to_task num_hyphens
+          _add_tag_with_points_value_to_task num_hyphens
           @_name_was_changed = true
         end
       end
@@ -70,24 +70,14 @@ module Epicify
       points
     end
 
-    def _add_tag_with_value_to_task tag_value
-      new_tag = Tag.create_with_points_value tag_value
-
+    def _add_tag_with_points_value_to_task points_value
+      new_tag = Tag.create_with_points_value points_value
       tags << new_tag
       @added_tags << new_tag
     end
 
     def _remove_any_preexisting_points_tags_from_task
-      return if tags.size == 0
-
-      all_tags_point_values = Tag::IDS_FOR_POINT_TAGS.keys.map(&:to_i)
-
-      all_tags_point_values.each do |removed_tag_point_value|
-        tag_id = Tag.tag_id_for_point_value removed_tag_point_value.to_s
-        if tags.map{|tag| tag["id"]}.include?(tag_id)
-          @removed_tags << Tag.new(tag_id)
-        end
-      end
+      @removed_tags = tags.select{|tag| tag.points > 0}
     end
 
   end
--- lib/epicify/tag.rb
+++ lib/epicify/tag.rb
@@ -1,28 +1,33 @@
 module Epicify
 
   class Tag < Struct.new(:id)
-    IDS_FOR_POINT_TAGS = { "1" => ENV["EPICIFY_STORY_POINTS_1_TAG_ID"].to_i, "2" => ENV["EPICIFY_STORY_POINTS_2_TAG_ID"].to_i, "3" => ENV["EPICIFY_STORY_POINTS_3_TAG_ID"].to_i}
+
+    def self.create_with_points_value points_value
+      tag_id = self.new.send :_tag_id_for_point_value, points_value
+      Tag.new(tag_id)
+    end
 
     def points
       _point_value_for_tag_id self.id
     end
 
-    def self.create_with_points_value points_value
-      tag_id = self.new.tag_id_for_point_value points_value.to_s
-      Tag.new(tag_id)
-    end
+    private
 
-    def tag_id_for_point_value point
-      IDS_FOR_POINT_TAGS[point]
-    end
+      def _ids_for_point_tags
+        {
+          1 => ENV["EPICIFY_STORY_POINTS_1_TAG_ID"].to_i,
+          2 => ENV["EPICIFY_STORY_POINTS_2_TAG_ID"].to_i,
+          3 => ENV["EPICIFY_STORY_POINTS_3_TAG_ID"].to_i
+        }
+      end
 
-    def self.tag_id_for_point_value point
-      IDS_FOR_POINT_TAGS[point]
-    end
+      def _tag_id_for_point_value point
+        _ids_for_point_tags[point]
+      end
 
-    def _point_value_for_tag_id id
-      IDS_FOR_POINT_TAGS.key(id).to_i
-    end
+      def _point_value_for_tag_id id
+        _ids_for_point_tags.key(id)
+      end
 
   end
 
